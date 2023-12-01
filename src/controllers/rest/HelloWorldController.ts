b7c1770ff98c72f63a713421a50e00e5ec7e80fd
Yaseen <yaseen@Yaseens-MacBook-Air.local>
2023-12-01 14:46:35 +0600

    Create Ts.ED project

---
+++ src/controllers/rest/HelloWorldController.ts
@@ -0,0 +1,11 @@
+import {Controller} from "@tsed/di";
+import {Get} from "@tsed/schema";
+
+@Controller("/hello-world")
+export class HelloWorldController {
+
+  @Get("/")
+  public get() {
+    return "hello";
+  }
+}
